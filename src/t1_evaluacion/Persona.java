393fe406817f41def57276ea9fea5b8812c9f323
WencaD <wencad18@gmail.com>
2023-09-09 13:19:14 -0500

    T1

---
+++ src/t1_evaluacion/Persona.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1_evaluacion;
+
+/**
+ *
+ * @author omarc
+ */
+public class Persona {
+    
+    private String datos;
+    
+    public Persona(String datos) {
+        this.datos = datos;
+    }
+    
+    public Persona(){       
+    }
+
+    public String getDatos() {
+        return datos;
+    }
+
+    public void setDatos(String datos) {
+        this.datos = datos;
+    }
+    
+}
---
+++ src/t1_evaluacion/T1_Evaluacion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1_evaluacion;
+
+/**
+ *
+ * @author omarc
+ */
+public class T1_Evaluacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FramePersona Frame = new FramePersona();
+        Frame.setVisible(true);
+        Frame.setLocationRelativeTo(null);
+    }
+    
+}
